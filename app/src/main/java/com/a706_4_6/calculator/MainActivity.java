21db521a546b9fe8bd01482f561f96711beb8242
zzMilkTEA <504267031@qq.com>
2018-12-12 02:31:04 +0800

    优化了内部逻辑，减少了一部分bug

--- app/src/main/java/com/a706_4_6/calculator/MainActivity.java
+++ app/src/main/java/com/a706_4_6/calculator/MainActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.util.regex.*;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button bt_0,bt_1,bt_2,bt_3,bt_4,bt_5,bt_6,bt_7,bt_8,bt_9,bt_pt,bt_prct,bt_AC,bt_bckspce,bt_plsmns,bt_pls,bt_mns,bt_mul,bt_div,bt_eq;
     TextView tv_frml,tv_rslt;
-    boolean clear_flag;         //clearflag默认为false
+    boolean clear_flag,operator_flag,operator_exist_flag;         //clearflag默认为false
+    String pattern = "(\\d+)(\\+|×|-|÷)(\\d+)";
+    Pattern patrn = Pattern.compile(pattern);    // 创建 Pattern 对象，判断符号用
+    Matcher m;// 创建 matcher 对象，输出判断
     //定义控件对象，通过接口方式实现事件的监听
 
     @Override
@@ -66,6 +70,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         String str = tv_frml.getText().toString();
+        if (Pattern.matches(pattern,str))operator_exist_flag=true;
+        else operator_exist_flag=false;
         switch (v.getId()) {
             case R.id.button_0:
             case R.id.button_1:
@@ -96,8 +102,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     str="";
                     tv_frml.setText("");
                 }
+                if(operator_exist_flag)break;
                 //判断用
-                tv_frml.setText(str + " " + ((Button) v).getText() + " ");
+                tv_frml.setText(str + ((Button) v).getText());
                 break;
             case R.id.button_AC:
                 if(clear_flag)
@@ -125,66 +132,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private void getresult()
+    private void getresult(){
+        String exp = tv_frml.getText().toString();
 
-
-    {
-        {
-            String exp = tv_frml.getText().toString();
-            if (exp == null || exp.equals("")) {
+        if (exp == null || exp.equals("")) {
                 return;
             }
-            if (!exp.contains(" ")) {
-                return;
-            }
-            if (clear_flag) {
+        if (clear_flag) {
                 clear_flag = false;
                 return;
             }
-            clear_flag = true;
-            double result = 0;
-            String s1 = exp.substring(0, exp.indexOf(" "));
-            String op = exp.substring(exp.indexOf(" ") + 1, exp.indexOf(" ") + 2);
-            String s2 = exp.substring(exp.indexOf(" ") + 3);
-            if (!s1.equals("") && !s2.equals("")) {
-                double d1 = Double.parseDouble(s1);
-                double d2 = Double.parseDouble(s2);
-                //强制类型转换
+        clear_flag = true;
+        double result = 0;
+        m= patrn.matcher(exp);
+       try {
+           m.find();
+           String s1 = m.group(1);
+           String op = m.group(2);
+           String s2 = m.group(3);
 
-                if (op.equals("+")) {
-                    result = d1 + d2;
-                } else if (op.equals("-")) {
-                    result = d1 - d2;
-                } else if (op.equals("×")) {
-                    result = d1 * d2;
-                } else if (op.equals("÷")) {
-                    if (d1 == 0) {
-                        result = 0;
-                    } else result = d1 / d2;
-                }
-                if (!s1.contains(".") && !s2.contains(".") && !op.equals("÷")) {
-                    int r = (int) result;
-                    tv_rslt.setText(r + "");
-                } else {
-                    tv_rslt.setText(result + "");
-                }
-            } else if (!s1.equals("") && s2.equals("")) {
-                tv_rslt.setText(exp);
-            } else if (s1.equals("") && !s2.equals("")) {
-                double d2 = Double.parseDouble(s2);
-                if (op.equals("+")) {
-                    result = 0 + d2;
-                } else if (op.equals("-")) {
-                    result = 0 - d2;
-                } else if (op.equals("×")) {
-                    result = 0 * d2;
-                } else if (op.equals("÷")) {
+        if (!s1.equals("") && !s2.equals("")) {
+            double d1 = Double.parseDouble(s1);
+            double d2 = Double.parseDouble(s2);
+            //强制类型转换
+
+            if (op.equals("+")) {
+                result = d1 + d2;
+            } else if (op.equals("-")) {
+                result = d1 - d2;
+            } else if (op.equals("×")) {
+                result = d1 * d2;
+            } else if (op.equals("÷")) {
+                if (d1 == 0) {
                     result = 0;
-                }
-            } else {
-                tv_frml.setText("");
+                } else result = d1 / d2;
             }
+
+            if (!s1.contains(".") && !s2.contains(".") && !op.equals("÷")) {
+                int r = (int) result;
+                tv_rslt.setText(r + "");
+            } else {
+                tv_rslt.setText(result + "");
+            }
+
+        } else if (!s1.equals("") && s2.equals("")) {
+            tv_rslt.setText(exp);
+        } else if (s1.equals("") && !s2.equals("")) {
+            double d2 = Double.parseDouble(s2);
+            if (op.equals("+")) {
+                result = 0 + d2;
+            } else if (op.equals("-")) {
+                result = 0 - d2;
+            } else if (op.equals("×")) {
+                result = 0 * d2;
+            } else if (op.equals("÷")) {
+                result = 0;
+            }
+        } else {
+            tv_frml.setText("");
         }
-    }
+       }catch(PatternSyntaxException e){
+           tv_rslt.setText("输入格式非法");
+       }
+        }
 }
 
